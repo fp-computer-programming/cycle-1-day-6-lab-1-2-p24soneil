037656417d33feac8069a62cbbcfed577d315f03
Sean O'Neil <p24soneil@fairfieldprep.org>
2023-09-19 13:47:42 -0400

    lab submission

--- lab_1-2.py
+++ lab_1-2.py
@@ -1,17 +1,17 @@
 # 1) Create a Python file named lab_1-2.py
 
 # 2) Add the heading to the first line of your file.
-
+<h1/>
 # 3) Create a variable and set it equal to your first name. Make sure the variable follows all the variable naming rules.
-
+5 = "sean"
 # 4) Create a print statement to display your variable when your code is run.
 
 # 5) Run the program.
-
+print (5)  
 # 6) Hit enter twice. Then hit Tab once or Space four times.
 
 # 7) Create a variable and set it equal to "your favorite hobby". Make sure the variable follows all the variable naming rules.
-
+    6 = "oneil" 
 # 8) Create a print statement to display your variable when your code is run.
-
+print (6)
 # 9) What happens when you run the program again?
